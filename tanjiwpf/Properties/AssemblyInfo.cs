d39abf398a91779751ff18a96f68ad6a99c7ea5b
b7c <58299468+b7c@users.noreply.github.com>
2020-02-18 22:16:52 +1300

    Initial commit

---
+++ tanjiwpf/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TanjiWPF")]
+[assembly: AssemblyDescription("A framework for building Tanji modules using WPF.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TanjiWPF")]
+[assembly: AssemblyCopyright("Copyright © b7 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e7246312-2de4-44a7-a53f-95a6b12d0902")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
